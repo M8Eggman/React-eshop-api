1ae7facccecd3ae6d45f1095bcc977624b96c832
GoNEAR <cagatay.morgul@outlook.com>
2025-06-04 09:57:35 +0200

    composant, page et route dans app créer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import './App.css'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import Details from "./pages/details/Details";
 
 export default function App() {
   return (
     <>
-
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<Details />} />
+        </Route>
+      </Routes>
     </>
   );
 }
---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,5 @@
+import "./Layout.css";
+
+export default function Layout() {
+  return <></>;
+}
